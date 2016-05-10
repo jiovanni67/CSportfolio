7b425bde659c0fc4b40a4a4b201f4f24510f681a
jiovanni67 <JRODRIGUEZ.EP@MYSUMMITPS.ORG>
2016-05-10 20:37:54 +0000

    update

--- interval2/calc.js
+++ interval2/calc.js
@@ -1,3 +1,4 @@
+// This part is for the button to work
 var button = document.getElementById("submitButton");
 var input1 = document.getElementById('input1');
 var input2 = document.getElementById('input2');
@@ -7,18 +8,44 @@ var dropdown = document.getElementById("dropdown");
 
 button.addEventListener("click", doMath);
 
+//This part function the math problem
 function doMath() {
+    //This part parasent inputs the values
     var num1 = parseInt(input1.value);
     var num2 = parseInt(input2.value);
     var operator = dropdown.value;
     
     
     // use this one as an example for the other 5 operations
+   
+   //This is for the addition to show up in the button and to answer the equation.
     if  (operator == "+"){
         answer = num1 + num2;
         display.innerHTML = answer;
     }
     
+    //This is for the subtract to show up in the button and to answer the equation.
+    if (operator == "-"){
+        answer = num1 - num2;
+        display.innerHTML = answer;
+    }
     
+    //This is for the multiplication to show up in the button and to answer the equation.
+    if (operator == "x"){
+        answer = num1 * num2;
+        display.innerHTML = answer;
+    }
     
+    //This is for the division to show up in the button and to answer the equation.
+    if (operator == "/"){
+        answer = num1 / num2;
+        display.innerHTML = answer;
+    }
+    
+    //This is for answering the problem and its going to appear in the button
+       if (operator == "%"){
+            answer = num1 % num2;
+            display.innerHTML = answer;
+           
+       }
 }
--- interval2/todolist.js
+++ interval2/todolist.js
@@ -1,15 +1,28 @@
+//This is the part were it gets the elements by id of the todolist, myform and newitem
 var todo = document.getElementById('todolist'),
     form = document.getElementById('myform'),
     field = document.getElementById('newitem');
-    
+// dropdown menu is a list of items that appear when clicking on a button or text selection   
+var dropdown = document.getElementById('dropdown');
+//This is for the color button were it says red,blue and green you can chage it
+var colors = document.getElementById('Colors');
+
+//This is for the submit button so it works
 form.addEventListener( 'submit', function(evt) {
   var text = field.value;
-  todo.innerHTML += '<div>' + text + '</div>';
+  todo.innerHTML += '<div style="color:' + dropdown.value + '">' + text + '</div>';
   evt.preventDefault();
 }, false);
 
-todo.addEventListener( 'click', function(evt) {
+
+
+//This crosses the things you write in the box when you submit it and touch it
+todo.addEventListener('click',function(evt){
   var tar = evt.target;
-  tar.parentNode.removeChild(tar);
-  evt.preventDefault();
+  if(tar.style.textDecoration === "line-through"){
+    tar.parentNode.removeChild(tar);
+  }else{
+    tar.style.textDecoration = "line-through"
+  }
+  evt.perventDefault();
 }, false);
