341d2fa36f0222872205ebbcb2eb65a59edaac96
jiovanni67 <JRODRIGUEZ.EP@MYSUMMITPS.ORG>
2016-05-11 17:48:12 +0000

    update

--- interval2/info.js
+++ interval2/info.js
@@ -3,12 +3,28 @@ var item2 = document.getElementById("item2");
 var info1 = document.getElementById("info1");
 var info2 = document.getElementById("info2");
 
+var info1 = document.getElementById("info1");
+var info2 = document.getElementById("info2");
+var info3 = document.getElementById("info3");
+var info4 = document.getElementById("info4");
+
+
+
 item1.addEventListener("click", function(){
-    info1.innerHTML = "Some stuff";
-    info2.innerHTML = "some other stuff";
+    info1.innerHTML = "<li>Huskies are born to run</li> <li> Their skills impressed Alaskans</li> <li>A lot of features help keep them warm</li> <li> But they're not great watchdogs</li>";
+    
 });
                        
 item2.addEventListener("click", function(){
-    info1.innerHTML = "Item 2 stuff";
-    info2.innerHTML = "Item 2 other stuff";
+    info1.innerHTML = "<li>Breeding is making them more gentle</li> <li> Dobermans are the fifth smartest breed and easily trained.</li> <li>Doberman dogs are war heroes </li> <li> Doberman pinschers are powerful, energetic dogs that need lots of exercise</li>";
+
+});
+item3.addEventListener("click", function(){
+    info1.innerHTML = "<li> Mustangs are very fast and thats the reason why lots of people like mustangs </li> Mostly teenagers likes to buy mustangs because their fast<li>22,000 Mustangs were sold within the first day of its release.</li> 2004 Ford produced its 300 millionth car, a Mustang GT convertible 40th anniversary edition. The 2004 models were the last cars built at Ford’s fabled Dearborn Assembly Plant which had been building Mustangs since the car’s introduction in 1964. <li> Theres a big number of people who have bought mustangs. Owners of mustangs say that they never regert on buying them they love their cars</li>";
+   
+   
+});
+item4.addEventListener("click", function(){ 
+    info1.innerHTML = "<li>The Dodge Charger is available in four different packages, and they are the SE, SXT, R/T, and SRT-8.</li>The Dodge Charger is built in Brampton, Ontario Canada. <li>The Dodge Charger was brought back in 2006 and is built on the LX platform along with the Chrysler 300, Dodge Challenger, and the discontinued Dodge Magnum.</li> <li>Doge charger cost from 27,995 in dealers</li> <li>Doge chargers are very nice looking that lots of people likes</li>";
+    
 });
